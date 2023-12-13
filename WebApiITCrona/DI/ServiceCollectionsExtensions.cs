7b442d69da3a3572ab76c2e713f2802166ea4e95
Sergey Letov <letovs87@gmail.com>
2023-12-13 22:31:21 +0300

    Добавление зависимостей в DI

--- WebApiITCrona/DI/ServiceCollectionsExtensions.cs
+++ WebApiITCrona/DI/ServiceCollectionsExtensions.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using WebApiITCrona.Context;
+using WebApiITCrona.Context.Abstract.Context;
+using WebApiITCrona.Services;
 
 namespace WebApiITCrona.DI;
 
+/// <summary>
+/// Расширение для <see cref="IServiceCollection"/>
+/// </summary>
 public static class ServiceCollectionsExtensions
 {
+    /// <summary>
+    /// Добавляет контекст БД
+    /// </summary>
     public static void AddCallStorageContext(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<CallStorageContext>(options => options.UseSqlServer(connectionString: configuration.GetConnectionString("Default"),
@@ -13,5 +21,19 @@ public static class ServiceCollectionsExtensions
                 opt.MigrationsHistoryTable("__EFMigrationsHistory");
                 opt.MigrationsAssembly(typeof(CallStorageContext).Assembly.GetName().Name);
             }));
+
+
+        services.AddScoped<ICallStorageContext>(serviceProvider => serviceProvider.GetRequiredService<CallStorageContext>());
+        services.AddScoped<IDbReader>(serviceProvider => serviceProvider.GetRequiredService<CallStorageContext>());
+        services.AddScoped<IDbWriter>(serviceProvider => serviceProvider.GetRequiredService<CallStorageContext>());
+        services.AddScoped<IUnitOfWork>(serviceProvider => serviceProvider.GetRequiredService<CallStorageContext>());
+    }
+
+    /// <summary>
+    /// Добавляет сервисы
+    /// </summary>
+    public static void AddServices(this IServiceCollection services)
+    {
+        services.AddScoped<IService, GeoService>();
     }
 }
