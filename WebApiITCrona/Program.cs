f05f88d023841deef14dc54648da8ec2aab9d5a1
Sergey Letov <letovs87@gmail.com>
2023-12-14 17:16:58 +0300

    Разбил program на создание хоста и конфигурирование хоста в Startup

--- WebApiITCrona/Program.cs
+++ WebApiITCrona/Program.cs
@@ -1,63 +1,39 @@
 using System.Reflection;
 using FluentValidation;
 using FluentValidation.AspNetCore;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using WebApiITCrona.DI;
 using WebApiITCrona.Infrastructure.Options;
 using WebApiITCrona.Infrastructure.Validators;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace WebApiITCrona;
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services
-    .AddFluentValidationAutoValidation(cfg =>
-    {
-        cfg.DisableDataAnnotationsValidation = false;
-    });
-builder.Services.AddValidatorsFromAssemblyContaining<IpRequestValidator>();
-
-builder.Services.AddSwaggerGen(options =>
+public class Program
 {
-    options.SwaggerDoc("v1", new OpenApiInfo
+    /// <summary>
+    /// Entry point
+    /// </summary>
+    public static async Task Main(string[] args)
     {
-        Title = "Web API",
-        Version = "v1"
-    });
-    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-});
+        await CreateHostBuilder(args, b => ConfigureWebHostBuilder(b))
+            .Build()
+            .RunAsync();
+    }
+    
+    /// <summary>
+    ///  Создает построитель хоста
+    /// </summary>
+    public static IHostBuilder CreateHostBuilder(string[] args, Action<IWebHostBuilder> webHostBuilderConfigurator)
+        => Microsoft.Extensions.Hosting.Host
+            .CreateDefaultBuilder()
+            .ConfigureWebHostDefaults(webHostBuilderConfigurator);
 
-builder.Services.AddCustomOptions(builder.Configuration);
-
-var options = builder.Services.BuildServiceProvider().GetRequiredService<IOptions<CustomHttpClientOptions>>();
-builder.Services.AddCustomHttpClient(options);
-
-builder.Services.AddCallStorageContext(builder.Configuration);
-
-builder.Services.AddServices();
-
-builder.Services.AddRepositories();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    /// <summary>
+    /// Создает построитель веб хост
+    /// </summary>
+    public static IWebHostBuilder ConfigureWebHostBuilder(IWebHostBuilder webHostBuilder)
+        => webHostBuilder
+            .UseStartup<Startup>();
 }
-
-app.UseHttpsRedirection();
-
-//app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ WebApiITCrona/Startup.cs
@@ -0,0 +1,89 @@
+﻿using System.Reflection;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using WebApiITCrona.DI;
+using WebApiITCrona.Infrastructure.Options;
+using WebApiITCrona.Infrastructure.Validators;
+
+namespace WebApiITCrona;
+
+/// <summary>
+/// Настройка веб хоста
+/// </summary>
+public class Startup
+{
+    /// <summary>
+    /// Конфигурации
+    /// </summary>
+    public IConfiguration Configuration { get; }
+
+    /// <summary>
+    /// ctor.
+    /// </summary>
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    /// <summary>
+    /// Конфигурация сервисов
+    /// </summary>
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+
+        services.AddFluentValidationAutoValidation(cfg =>
+        {
+            cfg.DisableDataAnnotationsValidation = false;
+        });
+        services.AddValidatorsFromAssemblyContaining<IpRequestValidator>();
+
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Web API",
+                Version = "v1"
+            });
+            var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+        });
+
+        services.AddCustomOptions(Configuration);
+
+        var options = services.BuildServiceProvider().GetRequiredService<IOptions<CustomHttpClientOptions>>();
+        services.AddCustomHttpClient(options);
+
+        services.AddCallStorageContext(Configuration);
+
+        services.AddServices();
+
+        services.AddRepositories();
+    }
+
+    /// <summary>
+    /// Конфигурация middlewear
+    /// </summary>
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        //app.UseAuthorization();
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
