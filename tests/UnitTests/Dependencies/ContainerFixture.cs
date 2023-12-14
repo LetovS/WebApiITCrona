08167a6ec534b5d9011dd1fea53393c31295d42d
Sergey Letov <letovs87@gmail.com>
2023-12-14 17:17:36 +0300

    Заготовка теста DI

---
+++ tests/UnitTests/Dependencies/ContainerFixture.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Hosting;
+using WebApiITCrona;
+using Xunit;
+
+namespace UnitTests.Dependencies;
+
+public class ContainerFixture : IDisposable
+{
+    private readonly IHost _host;
+    
+    /// <summary>
+    /// ctor.
+    /// </summary>
+    public ContainerFixture()
+    {
+        _host = Program.CreateHostBuilder(Array.Empty<string>(), b =>
+            {
+                var builder = Program.ConfigureWebHostBuilder(b);
+            })
+            .Build();
+    }
+    
+    public IServiceProvider Container => _host.Services;
+
+    /// <inheritdoc />
+    public void Dispose()
+    {
+        _host.Dispose();
+    }
+}
---
+++ tests/UnitTests/Dependencies/DependenciesTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using WebApiITCrona.Controllers;
+using Xunit;
+
+namespace UnitTests.Dependencies;
+
+public class DependenciesTests : IClassFixture<ContainerFixture>
+{
+    private readonly IServiceProvider _container;
+
+    /// <summary>
+    /// .ctor
+    /// </summary>
+    public DependenciesTests(ContainerFixture fixture)
+    {
+        _container = fixture.Container;
+
+        var t = _container.GetRequiredService(typeof(GeoController));
+    }
+
+    [Theory, MemberData(nameof(Controllers))]
+    internal void ControllerShouldBeResolved(Type controller)
+    {
+        var instance = _container.GetService(controller);
+
+        instance.Should().NotBeNull();
+    }
+    
+    /// <summary>
+    /// Список контроллеров Api
+    /// </summary>
+    public static IEnumerable<object[]> Controllers =>
+        typeof(GeoController).Assembly
+            .DefinedTypes
+            .Where(type => typeof(ControllerBase).IsAssignableFrom(type))
+            .Where(type => !type.IsAbstract)
+            .Select(type => new[] { type });
+}
