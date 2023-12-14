9a88df36b85a3467d631a85a63639583c7fa51d3
Sergey Letov <letovs87@gmail.com>
2023-12-14 15:14:47 +0300

    Реализован паттерн IOptions для получения настроек httpclient'a

---
+++ WebApiITCrona/Options/CustomHttpClientOptions.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApiITCrona.Options;
+
+/// <summary>
+/// Настройки для httpclient
+/// </summary>
+public sealed class CustomHttpClientOptions
+{
+    /// <summary>
+    /// Название http client
+    /// </summary>
+    public string HttpClientName { get; set; }
+    
+    /// <summary>
+    /// Базовый адрес
+    /// </summary>
+    public Uri UriBase { get; set; }
+}
