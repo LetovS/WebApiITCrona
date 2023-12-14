040fc4f31390bbcf4acbbacc66fed2588e05bcff
Sergey Letov <letovs87@gmail.com>
2023-12-14 15:15:07 +0300

    Реализована валидация формата ip адреса

---
+++ WebApiITCrona/Validators/IpRequestValidator.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using FluentValidation;
+using WebApiITCrona.Models;
+
+namespace WebApiITCrona.Validators;
+
+/// <summary>
+/// Валидация P адреса
+/// </summary>
+public class IpRequestValidator : AbstractValidator<IpRequest>
+{
+    /// <summary>
+    /// ctor.
+    /// </summary>
+    
+    public IpRequestValidator()
+    {
+        RuleFor(ip => ip.Ip)
+            .Cascade(CascadeMode.Stop)
+            .NotNull().WithMessage("IP-адрес не может быть пустым")
+            .NotEmpty().WithMessage("IP-адрес не может быть пустым")
+            .Must(BeValidIpAddress).WithMessage("Некорректный формат IP-адреса");
+    }
+
+    private static bool BeValidIpAddress(string ip)
+    {
+        IPAddress ipAddress;
+        return IPAddress.TryParse(ip, out _);
+    }
+}
